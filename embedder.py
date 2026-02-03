0db01645ee764e089656ea80262444a436c1bbc2
VivekChowdary9 <vivekchowdary_namburi@srmap.edu.in>
2026-02-03 09:29:50 +0530

    Fix SentenceTransformer encode argument

--- embedder.py
+++ embedder.py
@@ -1,8 +1,9 @@
 from sentence_transformers import SentenceTransformer
 
 class Embedder:
-    def __init__(self):
-        self.model = SentenceTransformer("all-MiniLM-L6-v2")
+    def __init__(self, model_name="all-MiniLM-L6-v2"):
+        self.model = SentenceTransformer(model_name)
 
     def embed(self, texts):
-        return self.model.encode(texts, convert_to_list=True)
+        embeddings = self.model.encode(texts)
+        return embeddings.tolist()
